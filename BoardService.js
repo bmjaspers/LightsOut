690444a6cda977ee742a7f48e59f49f5b97b8dc6
Ben <bmjaspers@gmail.com>
2015-04-30 00:08:58 -0400

    Add 2 new board sizes
    Add a 5x5 board to match the original game and a 9x9 board to bridge
    between 5x5 and 9x9.

--- BoardService.js
+++ BoardService.js
@@ -8,6 +8,8 @@
 
 		function _getPresetBoardSizes() {
 			return [
+				{ name: "5x5", width: 5, length: 5 },
+				{ name: "9x9", width: 9, length: 9 },
 				{ name: "11x11", width: 11, length: 11 },
 				{ name: "21x21", width: 21, length: 21 },
 				{ name: "51x51", width: 51, length: 51 }
